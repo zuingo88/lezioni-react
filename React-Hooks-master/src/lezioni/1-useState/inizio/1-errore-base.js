c6bc8eede4cc812ea02f67adb57878fad22badd9
valerio sgura <valeriosgura88@gmail.com>
2021-10-13 11:46:21 +0200

    useState basi.

--- React-Hooks-master/src/lezioni/1-useState/inizio/1-errore-base.js
+++ React-Hooks-master/src/lezioni/1-useState/inizio/1-errore-base.js
@@ -2,7 +2,23 @@ import React from "react";
 //Far vedere che il componente non si riaggiorna ma la variabile si
 //Spiegare che cosa è un React Fragment e che è possibile abbreviarlo
 const ErroreBase = () => {
-  return <div>Error Components</div>;
+
+  let titolo = `Hello world!`;
+
+  const cambiaTitolo = () => {
+    titolo = `Nuovo Titolo`;
+    console.log(titolo);
+  };
+
+  return <>
+    <h2>{titolo}</h2>
+    <button
+      className="btn btn-info my-3"
+      onClick={cambiaTitolo}
+    >
+      Change me!
+    </button>
+  </>;
 };
 
 export default ErroreBase;
--- React-Hooks-master/src/lezioni/1-useState/inizio/2-useState-basi.js
+++ React-Hooks-master/src/lezioni/1-useState/inizio/2-useState-basi.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 // Gli Hooks devono essere usasti con use davanti
 // componenti devono avere la prima lettere Maiuscola
 // Gli Hook devono essere invocati all'interno del corpo/funzione del componente
 // Gli Hook non possono essere utilizzati in maniera condizionale
 
 const UsoBase = () => {
-  //   const value = useState()[0];
-  //   const handler = useState()[1];
-  //   console.log(value, handler);
+
+  //  const { log } = console;
+  //  log(useState(`Hello World!`));
+  //  const value = useState()[0];
+  //  const handler = useState()[1];
+  //  log(`Value = `, value, `Handler = `, handler);
+
+  const [titolo, setTitolo] = useState(`Hello World!!!`);
+
+  const changeTitolo = () => {
+    if (titolo === 'Hello World!!!') {
+      setTitolo(`Nuovo Titolo!`);
+    } else {
+      setTitolo(`Hello World!!!`);
+    }
+
+  };
 
   return (
+
     <>
-      <h2> Basi useState</h2>
+      <h2>{titolo}</h2>
+      <button
+        type='button'
+        className='btn btn-info shadow my-3'
+        onClick={changeTitolo}
+      >
+        Change Me!
+      </button>
     </>
   );
 };
