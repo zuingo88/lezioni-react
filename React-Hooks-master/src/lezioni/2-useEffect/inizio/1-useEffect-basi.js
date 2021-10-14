de4555b6e57b3591459ba38e1d303be11c418fbb
valerio sgura <valeriosgura88@gmail.com>
2021-10-14 10:34:27 +0200

    useEffect - Chiamata cleanup function.

--- React-Hooks-master/src/lezioni/2-useEffect/inizio/1-useEffect-basi.js
+++ React-Hooks-master/src/lezioni/2-useEffect/inizio/1-useEffect-basi.js
@@ -2,10 +2,14 @@ import React, { useEffect } from "react";
 
 const useEffectBasi = () => {
 
-  const [value, setValue] = React.useState(0); // Altro modo per dichiarare Hooks useState
+  const [value, setValue] = React.useState(0); // Altro modo per dichiarare Hooks useState.
 
   const aumenta = () => {
     setValue((oldValue) => oldValue + 1);
+  }
+
+  const secondEffect = () => {
+    console.log(`Sono il secondo useEffect`);
   }
 
   useEffect(() => {
@@ -15,7 +19,12 @@ const useEffectBasi = () => {
     } else {
       document.title = `${value} Nuovi messaggi`;
     }
+
+    return () => console.log(`CleanUp function chiamata`); // Questa funzione non viene eseguita al caricamento della pagina, ma solo alla prima variazione di value (in questo caso). 
+
   }, [value]); // Lo useEffect sarà eseguito ogni qual volta ci sarà una variazione dell'elemento scritto dentro l'array. In caso di array vuoto lo useEffect sarà eseguito solo al primo caricamento della pagina.
+
+  useEffect(secondEffect, []);
 
   return (
     <div>
