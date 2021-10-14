b2cc85dba31ad5dfcd0afc44d462d3b167000e4c
valerio sgura <valeriosgura88@gmail.com>
2021-10-14 18:21:00 +0200

    useEffect - Data Fetching.

--- React-Hooks-master/src/App.js
+++ React-Hooks-master/src/App.js
@@ -1,4 +1,4 @@
-import Inizio from "./lezioni/2-useEffect/inizio/2-useEffect-cleanup"; 
+import Inizio from "./lezioni/2-useEffect/inizio/3-useEffect-fetch-data"; 
 
 function App() {
   return (
--- React-Hooks-master/src/lezioni/2-useEffect/inizio/3-useEffect-fetch-data.js
+++ React-Hooks-master/src/lezioni/2-useEffect/inizio/3-useEffect-fetch-data.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
 const url = "https://api.github.com/users";
 
 const FetchComponent = () => {
+
+  const [users, setUsers] = useState([]);     //  Dichiaro come state un array vuoto.
+
+  const getData = async () => {               //  Funzione per fetchare dati tramite Axios.
+    const response = await Axios.get(url);
+    console.log(response);
+    setUsers(response.data);                  //  setto nuovo state con array di oggetti ricevuto da risposta chiamata Axios di tipo GET.
+  }
+
+  useEffect(() => {                           //  Richiamo la funzione get useEffect
+    getData();                                //  Richiamo la funzione getata in useEffect perchè esso non può avere come parametro un async.
+  }, []);                                     //  Do un secondo parametro a useEffect per far sì che la chiamata Axios avvenga una sola vola al caricamento della pagina.
+
   return (
     <div>
       <h1>Fetch Component</h1>
+      <ul className='users'>
+
+        {users.map((el) => {
+          const { login, id, avatar_url: img, html_url } = el
+          return (
+            <li key={id} className='shadow'>
+              <img src={img} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>Link</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 };
 
 export default FetchComponent;
