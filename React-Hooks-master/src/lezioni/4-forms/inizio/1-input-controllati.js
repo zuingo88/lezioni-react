b68a63e62babbf4160daff74805fbe111b77a090
valerio sgura <valeriosgura88@gmail.com>
2021-10-15 19:16:08 +0200

    Form submit.

--- React-Hooks-master/src/lezioni/4-forms/inizio/1-input-controllati.js
+++ React-Hooks-master/src/lezioni/4-forms/inizio/1-input-controllati.js
@@ -1,23 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ControlledInput = () => {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [tel, setTel] = useState('');
+  const [users, setUsers] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (name && email && tel) {
       console.log('Form inviato!', 'Nome:', name, 'Email:', email, 'Telefono:', tel);
+
+      setUsers(
+        [
+          ...users,
+          {
+            id: new Date(Date.now()).getTime().toString(),  //  Trickc per generare un numero univoco. L'ideale è usare libreerie come uuid.
+            name, //  Equivalente di scrivere `nome: nome,`. In ES6 si può fare quando la chiave è uguale al valore che gli viene assegnato.
+            email,
+            tel,
+          }
+        ]
+      )
+
       setName('');
       setEmail('');
-      setTel('');      
+      setTel('');
     } else {
       alert('Riempi i campi richiesti!');
     }
-  }
+  };
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
 
   return (
     <form className='bg-white shadow p-4 rounded'>
