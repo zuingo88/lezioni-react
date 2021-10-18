5276a9984c776e3ccb52c6af49d50d012058f6c4
valerio sgura <valeriosgura88@gmail.com>
2021-10-18 19:38:07 +0200

    prop-drilling.

--- React-Hooks-master/src/lezioni/7-prop-drilling/inizio/prop-drilling.js
+++ React-Hooks-master/src/lezioni/7-prop-drilling/inizio/prop-drilling.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from '../../../data'
 
 const MainComponent = () => {
+
+  const [people, setPeople] = useState(data);
+  
+  const removePeople = (id) => setPeople(people.filter(el => el.id !== id));
+
   return (
     <div>
       <h2>Passaggio di Proprietà a Cascata</h2>
+      <Elenco
+        people={people}
+        removePeople={removePeople}
+      />
     </div>
+  );
+};
+
+const Elenco = ({ people, removePeople }) => {
+  return (
+    <div>
+      {people.map((el, index) => {
+        return (
+          <Persona
+            key={el.id}
+            {...el}
+            removePeople={removePeople} />
+        )
+      })}
+    </div>
+  );
+};
+
+const Persona = ({ name, id, removePeople }) => {
+  return (
+    <article className='item shadow'>
+      <h5>{name}</h5>
+      <button
+        className="button delete-button"
+        onClick={() => removePeople(id)}
+      >
+        Elimina
+      </button>
+    </article>
   );
 };
 
