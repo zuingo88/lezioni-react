9a78407f32d6d2c2dc9c5c43f0b8296be96e25a4
valerio sgura <valeriosgura88@gmail.com>
2021-10-19 11:08:04 +0200

    PropTypes - Valori di default.

--- React-Hooks-master/src/lezioni/10-prop-types/inizio/prop-types.js
+++ React-Hooks-master/src/lezioni/10-prop-types/inizio/prop-types.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { datiIncompleti } from "../../../data";
+import PropTypes from "prop-types";
+
+const defaultImage =
+  "https://dl.airtable.com/.attachments/5b3ad76dfd6ca5e31810cb99141c7ede/69829b2f/pexels-dominika-roseclay-1139785.jpg";
+
 
 const PropComponent = () => {
   return (
@@ -15,11 +20,13 @@ const PropComponent = () => {
   );
 };
 
-const Prodotto = ({nome, prezzo, image}) => {
+const Prodotto = ({ nome, prezzo, image }) => {
+  const img = image && image.url  //  Se image è true allora sarà uguale a image.url.
+
   return (
     <article className='card shadow my-3 p-2'>
       <div className="card-body">
-        <img src={image.url} alt={nome} className="card-img-top" />
+        <img src={img || defaultImage} alt={nome} className="card-img-top" />
         <div className="card-text">
           <h4 className="card-title">
             {nome}
@@ -31,6 +38,17 @@ const Prodotto = ({nome, prezzo, image}) => {
       </div>
     </article>
   )
+}
+
+Prodotto.propTypes = {
+  nome: PropTypes.string.isRequired,
+  prezzo: PropTypes.number.isRequired,
+  image: PropTypes.object.isRequired,
+}
+
+Prodotto.defaultProps = {
+  nome: 'divano default',
+  prezzo: 17.99,
 }
 
 export default PropComponent;
