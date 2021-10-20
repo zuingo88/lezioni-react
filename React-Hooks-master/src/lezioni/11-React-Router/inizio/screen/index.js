dc03ecdee515f51020146030209d25092f3e10ca
valerio sgura <valeriosgura88@gmail.com>
2021-10-20 12:25:28 +0200

    Router - Router, Route e Switch.

--- React-Hooks-master/src/lezioni/11-React-Router/inizio/screen/index.js
+++ React-Hooks-master/src/lezioni/11-React-Router/inizio/screen/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const index = () => {
-  return <div>React Router</div>;
+  return (
+    <Router>
+      <h1>React Router</h1>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/About' component={About} />
+      </Switch>
+    </Router>
+
+  );
 };
 
 export default index;
