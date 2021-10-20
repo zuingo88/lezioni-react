c31eb737ea8123d1d609635766db287650582220
valerio sgura <valeriosgura88@gmail.com>
2021-10-20 14:31:08 +0200

    Router - Link ed ErrorPage.

---
+++ React-Hooks-master/src/lezioni/11-React-Router/inizio/screen/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <h1>
+      Error!!!
+    </h1>
+  )
+}
+
+export default ErrorPage
--- React-Hooks-master/src/lezioni/11-React-Router/inizio/screen/index.js
+++ React-Hooks-master/src/lezioni/11-React-Router/inizio/screen/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import About from "./About";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 const index = () => {
   return (
     <Router>
       <h1>React Router</h1>
+      <ul className='nav'>
+        <li className='nav-item'>
+          <Link to="/" className='nav-link'>Home</Link>
+        </li>
+        <li className='nav-item'>
+          <Link to="/About" className='nav-link'>About</Link>
+        </li>
+        <li className='nav-item'>
+          <Link to="/Progetti" className='nav-link'>Progetti</Link>
+        </li>
+      </ul>
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/About' component={About} />
+        <Route path='*' component={ErrorPage} />
       </Switch>
     </Router>
 
